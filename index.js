8db6c9374fd9f4ebab5960ade8ae0e2530fd2ff8
khamzat <khamz@mail.ru>
2021-12-16 11:59:18 +0300

    добавил загрузчик файла

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
-app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
+app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
 
 app.use(require('./routes'));
 
 const connectAndStartServer = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_SERVER, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(process.env.MONGO_SERVER, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        app.listen(process.env.PORT, () => {
-            console.log(chalk.green(`Успешно соединились. Порт ${process.env.PORT}`));
-        });
-    } catch (e) {
-        console.log(chalk.bgRed.white(`Ошибка при подключении: ${e.toString()}`));
-    }
+    app.listen(process.env.PORT, () => {
+      console.log(chalk.green(`Успешно соединились. Порт ${process.env.PORT}`));
+    });
+  } catch (e) {
+    console.log(chalk.bgRed.white(`Ошибка при подключении: ${e.toString()}`));
+  }
 };
 
 connectAndStartServer();

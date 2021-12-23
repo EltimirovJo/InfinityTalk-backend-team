328de57dc36fa56bfac7b067c2fb70288a99c6c7
Alimkhan8195 <alimkhan.salamov@gmail.com>
2021-12-23 23:21:40 +0300

    backend for websites and apps

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = Router();
 router.use('/users', require('./users.route'));
 router.use('/languages', require('./languages.route'));
 router.use('/chats', require('./chats.route'));
+router.use('/utilities', require('./utilities.route'));
 
 module.exports = router;
